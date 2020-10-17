7bf3c45e36e8a3d86faa9d7d632a7713e0b8818d
AndreyFrolov44 <andre.frolov44@gmail.com>
2020-10-18 00:44:15 +0300

    create models

--- malefashion/fashion/admin.py
+++ malefashion/fashion/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
-# Register your models here.
+from .models import *
+
+
+class CommentInline(GenericTabularInline):
+    model = Comment
+
+
+class ImageInline(GenericTabularInline):
+    model = ProductImage
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'brand', 'quantity', 'available',)
+    list_editable = ('price', 'category', 'brand', 'quantity', 'available',)
+    fields = (
+        'name',
+        ('price', 'category', 'brand',),
+        'description_lite',
+        'description',
+        'additional_information',
+        ('quantity', 'available',),
+        'slug',
+    )
+    inlines = [
+        CommentInline,
+        ImageInline,
+    ]
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(ProductSize)
+class ProductSizeAdmin(admin.ModelAdmin):
+    list_display = ('value',)
+
+
+@admin.register(RatingStar)
+class RatingStarAdmin(admin.ModelAdmin):
+    list_display = ('value',)
+
+
+@admin.register(Rating)
+class RatingStarAdmin(admin.ModelAdmin):
+    list_display = ('ip', 'star', 'product',)
+
+
+@admin.register(RelatedProduct)
+class RatingStarAdmin(admin.ModelAdmin):
+    list_display = ('product', 'related',)
+
+
+@admin.register(Article)
+class RatingStarAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date',)
+    inlines = [
+        CommentInline,
+    ]
+
+
+
--- malefashion/fashion/models.py
+++ malefashion/fashion/models.py
@@ -1,3 +1,101 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=70)
+    slug = models.SlugField(max_length=70)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=70)
+    slug = models.SlugField(max_length=70)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    description_lite = models.TextField()
+    description = models.TextField()
+    additional_information = models.TextField()
+    quantity = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=210)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product')
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return self.product
+
+
+class ProductSize(models.Model):
+    value = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.value
+
+
+class RatingStar(models.Model):
+    value = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return self.value
+
+
+class Rating(models.Model):
+    ip = models.CharField(max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CharField)
+
+    def __str__(self):
+        return self.product
+
+
+class RelatedProduct(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    related = models.ForeignKey(Product, on_delete=models.CharField, related_name='related')
+
+    def __str__(self):
+        return self.product
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    slug = models.SlugField(max_length=210)
+    draft = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    text = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content_type
